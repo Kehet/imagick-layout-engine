1b901ff4b995c1114b062abe0e096d59d50a401a
Kehet <Kehet@users.noreply.github.com>
2025-07-20 10:43:31 +0300

    Ran the config generator

---
+++ src/ImagickLayoutEngineClass.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace Kehet\ImagickLayoutEngine;
+
+class ImagickLayoutEngineClass {}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Kehet\ImagickLayoutEngine\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase {}
